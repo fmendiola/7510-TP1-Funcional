0e0bf90992c3cf4c3947bec3dae6c30993c710f3
Franco Mendiola <mendiola.f92@hotmail.com>
2017-09-08 02:51:07 -0300

    test "database incompleto" terminado

--- src/logical_interpreter.clj
+++ src/logical_interpreter.clj
@@ -1,7 +1,43 @@
 (ns logical-interpreter)
+(require '[clojure.string :as str])
+
+(def fact-pattern #"[a-zA-Z]*\(.*\)\." ) ;revisar REGEX
+
+(defn recognise-match
+  [regex row]
+  (not (nil? (re-matches regex row))) ;re-matches o re-find
+  )
+
+(defn is-fact-pattern
+  [reg]
+  (if (true? (recognise-match fact-pattern reg) ) ; redefinir la comparacion
+    true
+    false
+    )
+  )
+
+(defn function-or ;funcion OR
+  [x y]
+  (or x y )
+  )
+
+(defn is-complete-db
+  [db]
+  ;(println db)
+  ;(println(re-matches fact-pattern"varon(juan,diego,pepe,dario)."))
+  (if (empty? db)
+    false
+    (reduce function-or (map is-fact-pattern db ))
+
+    )
+  )
 
 (defn evaluate-query
   "Returns true if the rules and facts in database imply query, false if not. If
   either input can't be parsed, returns nil"
   [database query]
-  nil)
+  (def db-vector (vec (str/split database #"\s+")))
+  (if (is-complete-db db-vector)
+    (println "LLLLLUEEGUEEEEE!!");proceso
+    nil)
+  )
